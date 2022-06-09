a022298e7ea92474688b9239d60139976f529ec7
Renan Aragão <refaragao@pop-os.localdomain>
2022-06-08 22:37:36 -0300

    install rocket

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
+#[macro_use] extern crate rocket;
+
 mod models;
 mod repository;
 mod telegram;
 
 use uri_builder::URI;
 
-#[tokio::main]
-async fn main() {
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+
     let token = std::env::var("TELEGRAM_TOKEN").or(Err("TELEGRAM_TOKEN environment variable missing")).unwrap();
     let path = format!("bot{token}", token = token).to_string();
+    let _uri = URI::new("https").host("api.telegram.org").path(&path);
 
-    telegram::send_message::post(
-        URI::new("https").host("api.telegram.org").path(&path),
-        638061488,
-        "asdasd",
-    )
-    .await
-    .unwrap();
-
-    println!("Finished");
+    rocket::build().mount("/", routes![index])
 }
